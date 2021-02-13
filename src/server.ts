367bb2e32f40fcb5373aaa4efe3d549bf7714504
Marcio Flavio <mflaviof1995@gmail.com>
2021-02-13 14:14:49 -0300

    Changing port to env

--- src/server.ts
+++ src/server.ts
@@ -5,15 +5,13 @@ import routes from "./routes";
 
 dotenv.config();
 
-const port = 8081;
-
 (async () => {
   const app = express();
   app.use(express.json());
   app.use(routes);
 
   await createConnection();
-  app.listen(port, () => {
-    console.log(`server started at http://localhost:${port}`);
+  app.listen(process.env.PORT || 8081, () => {
+    console.log(`server started`);
   });
 })();
